3e42d10b8fc538adbc032110e62df1ac127fe191
Thomas Jensen <thomasjensen1194@gmail.com>
2019-06-24 23:24:55 +0200

    Added helmet (not working)

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,13 @@
 import express from "express";
 import "reflect-metadata";
 import apollo from "./graphql/apolloServer";
+import helmet from "helmet";
 import "./typeORM/typeORM";
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(helmet());
 apollo.applyMiddleware({ app });
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running in http://localhost:${PORT}`);
